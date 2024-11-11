97d172123f53cf3a792c41bc582120225fa465f4
Patrick Chester <pjc468@nyu.edu>
2024-11-10 20:15:55 -0800

    Remove copilot

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -610,19 +610,19 @@ local plugin_specs = {
     ft = "lua", -- only load on lua files
     opts = {},
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+  --{
+  --  "CopilotC-Nvim/CopilotChat.nvim",
+  --  branch = "canary",
+  --  dependencies = {
+  --    { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+  --    { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+  --  },
+  --  opts = {
+  --    debug = true, -- Enable debugging
+  --    -- See Configuration section for rest
+  --  },
+  --  -- See Commands section for default commands if you want to lazy load on them
+  --},
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
