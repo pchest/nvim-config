04a7e608d337838eb16ec69f9264efb32b5ea111
Patrick Chester <patrickjchester@gmail.com>
2025-10-28 20:52:51 -0400

    Add quarto snippets and config

---
+++ lua/config/quarto.lua
@@ -0,0 +1,2 @@
+vim.keymaps.set("n", "<leader>r9", ":QuartoPreview<CR>", { desc = "Quarto Preview" })
+vim.keymaps.set("n", "<leader>r0", ":QuartoClosePreview<CR>", { desc = "Quarto Close Preview" })
