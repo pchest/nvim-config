834488ee1154fa3f87966fe0c890a69203434397
Patrick Chester <pjc468@nyu.edu>
2025-11-20 17:49:08 -0500

    Add surround word functionality

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -52,6 +52,16 @@ local plugin_specs = {
       require("config.nvim-cmp")
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
   -- {
   --   "saghen/blink.cmp",
   --   -- optional: provides snippets for the snippet source
