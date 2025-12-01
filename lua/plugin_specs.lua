baf66315889ee04945ab99aeeb4a0fd0bf4e8ea4
Patrick Chester <pjc468@nyu.edu>
2025-11-30 19:33:21 -0500

    Remove code companion due to breaking changes

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -704,23 +704,23 @@ local plugin_specs = {
       },
     },
   },
-  {
-   "olimorris/codecompanion.nvim",
-   dependencies = {
-     "nvim-lua/plenary.nvim",
-     "nvim-treesitter/nvim-treesitter",
-     "hrsh7th/nvim-cmp",
-     "nvim-telescope/telescope.nvim",
-     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
-     { "stevearc/dressing.nvim", opts = {} },
-   },
-   opts = {
-     keymaps = {
-       close = "<C-w>q",
-     },
-    },
-    config = true,
-   },
+  --{
+  -- "olimorris/codecompanion.nvim",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "nvim-treesitter/nvim-treesitter",
+  --   "hrsh7th/nvim-cmp",
+  --   "nvim-telescope/telescope.nvim",
+  --   { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
+  --   { "stevearc/dressing.nvim", opts = {} },
+  -- },
+  -- opts = {
+  --   keymaps = {
+  --     close = "<C-w>q",
+  --   },
+  --  },
+  --  config = true,
+  -- },
 --{
   { 'github/copilot.vim' },
   --{
