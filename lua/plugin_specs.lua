5340fda0c109807736ec2dcf0cf17d3aaefcc889
Patrick Chester <pjc468@nyu.edu>
2026-03-01 16:38:39 -0500

    Comment out sidekick

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -52,6 +52,83 @@ local plugin_specs = {
       require("config.nvim-cmp")
     end,
   },
+  --{
+  --  "folke/sidekick.nvim",
+  --  opts = {
+  --    -- add any options here
+  --    cli = {
+  --      mux = {
+  --        backend = "zellij",
+  --        enabled = true,
+  --      },
+  --    },
+  --  },
+  --  keys = {
+  --    {
+  --      "<tab>",
+  --      function()
+  --        -- if there is a next edit, jump to it, otherwise apply it if any
+  --        if not require("sidekick").nes_jump_or_apply() then
+  --          return "<Tab>" -- fallback to normal tab
+  --        end
+  --      end,
+  --      expr = true,
+  --      desc = "Goto/Apply Next Edit Suggestion",
+  --    },
+  --    {
+  --      "<c-.>",
+  --      function() require("sidekick.cli").toggle() end,
+  --      desc = "Sidekick Toggle",
+  --      mode = { "n", "t", "i", "x" },
+  --    },
+  --    {
+  --      "<leader>sa",
+  --      function() require("sidekick.cli").toggle() end,
+  --      desc = "Sidekick Toggle CLI",
+  --    },
+  --    {
+  --      "<leader>ss",
+  --      function() require("sidekick.cli").select() end,
+  --      -- Or to select only installed tools:
+  --      -- require("sidekick.cli").select({ filter = { installed = true } })
+  --      desc = "Select CLI",
+  --    },
+  --    {
+  --      "<leader>sd",
+  --      function() require("sidekick.cli").close() end,
+  --      desc = "Detach a CLI Session",
+  --    },
+  --    {
+  --      "<leader>st",
+  --      function() require("sidekick.cli").send({ msg = "{this}" }) end,
+  --      mode = { "x", "n" },
+  --      desc = "Send This",
+  --    },
+  --    {
+  --      "<leader>sf",
+  --      function() require("sidekick.cli").send({ msg = "{file}" }) end,
+  --      desc = "Send File",
+  --    },
+  --    {
+  --      "<leader>sv",
+  --      function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+  --      mode = { "x" },
+  --      desc = "Send Visual Selection",
+  --    },
+  --    {
+  --      "<leader>sp",
+  --      function() require("sidekick.cli").prompt() end,
+  --      mode = { "n", "x" },
+  --      desc = "Sidekick Select Prompt",
+  --    },
+  --    -- Example of a keybinding to open Claude directly
+  --    {
+  --      "<leader>sc",
+  --      function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
+  --      desc = "Sidekick Toggle Claude",
+  --    },
+  --  },
+  --},
   -- {
   --   "saghen/blink.cmp",
   --   -- optional: provides snippets for the snippet source
